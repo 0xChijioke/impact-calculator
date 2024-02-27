3072e7aa6a2f333f06d909942c0dee3595b900e3
EngrGord <chijokeogbuanya@gmail.com>
2024-02-27 14:16:50 +0100

    rename func to GET and remove default

--- packages/nextjs/app/api/impact/route.ts
+++ packages/nextjs/app/api/impact/route.ts
@@ -11,7 +11,7 @@ interface VectorWeight {
   weight: number;
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "GET") {
     return res.status(405).json({ message: "Method not allowed." });
   }
